3f9ec630286e2c8eb12397adc26a19c30d29c9a7
budiboy <budiboy195@gmail.com>
2021-04-15 00:24:40 +0700

    code

--- controller/absenController.js
+++ controller/absenController.js
@@ -1,25 +1,90 @@
 const express = require("express");
 const absen = express.Router();
-const {tambahAbsen} = require("../model/absenModel");
+const {
+  tambahAbsen,
+  tampilAbsen,
+  hapusAbsen,
+  editAbsen
+} = require("../model/absenModel");
 
 // tangkap data dari request body (adanya di test.res)
 absen.post("/", (req, res) => {
-    let data = req.body;
+  let data = req.body;
 
-    // masukan fungsinya
-    tambahAbsen(data)
-    .then((result)=>{
-        res.json({
-            message: "data berhasil dimasukan",
-            rows: result,
-        })
-    .catch((err)=> {
-        res.status(400).json({
-            pesan: "eror pada mysql syntax",
-        });
-    });
+  // masukan fungsinya
+  tambahAbsen(data)
+    .then((result) => {
+      res.json({
+        message: "data berhasil dimasukan",
+        rows: result,
+      });
+    })
+    .catch((err) => {
+      res.status(400).json({
+        pesan: "eror pada mysql syntax",
+      });
     });
 });
 
+// tangkap data dari request body(test,res)
+absen.get("/", (req, res) => {
+  // functiom tampil model
+  tampilAbsen()
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(404).json({
+        pesan: "tidak bisa menampilkan data",
+      });
+    });
+});
+
+// tangkp data dari request body
+absen.delete("/", (req, res) => {
+  let data = req.body;
+
+  // function hapusAbsen
+  hapusAbsen(data)
+    .then((result) => {
+      if (result > 0) {
+        res.status(200).json({
+          pesan: "berhasil menghapus data",
+        });
+      } else {
+        res.status(404).json({
+          pesan: "data yang akan dihapus tidak ditemukan",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(401).json({
+        pesan: "gagal menghapus data",
+      });
+    });
+});
+
+// function editAbsen
+absen.patch("/edit/:id", (req, res) => {
+  let id = req.params.id; //capture params id
+  let data = req.body; //capture body dta
+
+  //   buat function editabsen
+  editAbsen(id, data)
+    .then((result) => {
+      if (result > 0) {
+        res.status(200).json({
+          hasil: result,
+          pesan: "berhasil ubah data",
+        });
+      } else {
+        res.status(404).json({
+          hasil: result,
+          pesan: "data yang akan diubah tidak ditemukan",
+        });
+      }
+    })
+    .catch((err) => console.log(err));
+});
 
 module.exports = absen;
--- model/absenModel.js
+++ model/absenModel.js
@@ -12,4 +12,42 @@ const tambahAbsen = async (data) => {
     .catch((err) => console.log(err));
 };
 
-module.exports = { tambahAbsen };
+
+// membuat function tampil absen
+const tampilAbsen = async () => {
+    return await db
+    .from("siswa")
+    .select("*")
+    .then((rows) => {
+        return rows;
+    })
+    .catch((err) => console.log(err));
+};
+
+// function hapusAbsen
+const hapusAbsen = async (data)  => {
+    return await db("siswa")
+    .where(data)
+    .del()
+    .then((rows) => {
+        return rows;
+    })
+    .catch((err) => console.log(err))
+};
+
+// function edit absen
+const editAbsen = async (id, data) => {
+    return await db("siswa")
+    .where({ id: id })
+    .update(data)
+    .then((rows) => {
+        return rows;
+    })
+    .catch((err) => console.log(err))
+}
+
+
+// membuat fuction hapusAbsen
+// membuat fuction editAbsen
+
+module.exports = { tambahAbsen, tampilAbsen, hapusAbsen, editAbsen };
